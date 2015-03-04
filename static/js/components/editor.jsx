7674f78e4b0572de19887490d2f70fa19211fc83
Gio d'Amelio <giodamelio@gmail.com>
2015-03-04 01:14:17 -0800

    Set ace theme from the settings store

--- static/js/components/editor.jsx
+++ static/js/components/editor.jsx
@@ -1,12 +1,16 @@
 var React = require("react");
-var ace = require("brace");
+
+var Store = require("../store");
 
 module.exports = React.createClass({
+    mixins: [Store.mixin],
+    cursor: ["settings"],
     render() {
+        console.log();
         var style = {
             position: "absolute",
             width: "100%",
-            height: (window.innerHeight - 50) + "px",
+            height: "calc(100% - 50px)",
             top: "50px"
         };
         return <div id="editor" style={style}></div>;
@@ -15,7 +19,17 @@ module.exports = React.createClass({
         var element = this.getDOMNode();
         
         // Create our ace editor
-        ace.edit(element);
+        var editor = ace.edit(element);
+
+        // Set it to rust mode
+        editor.getSession().setMode("ace/mode/rust");
+
+        // Set the theme from the settings
+        var themepath = this.cursor.select("theme", "theme").get();
+        editor.setTheme(themepath);
+
+        // Get a list of all the ace themes
+        var themelist = ace.require("ace/ext/themelist");
     }
 });
 
--- static/js/store.js
+++ static/js/store.js
@@ -1,6 +1,15 @@
 var baobab = require("baobab");
 
-var store = new baobab({});
+var store = new baobab({
+    settings: {
+        theme: {
+            caption: "Monokai",
+            isDark: true,
+            name: "monokai",
+            theme: "ace/theme/monokai"
+        }
+    }
+});
 
 module.exports = store;
 
