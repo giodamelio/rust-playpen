9410d332e5e7e77ba0fe1ef8c8386cc05f12f0ea
Gio d'Amelio <giodamelio@gmail.com>
2015-03-03 23:30:45 -0800

    Add buttons to the navbar

--- static/js/components/nav.jsx
+++ static/js/components/nav.jsx
@@ -1,24 +1,33 @@
 var React = require("react");
 
-var {Navbar, Nav, NavItem, DropdownButton, MenuItem} = require("react-bootstrap");
+var {
+    Navbar,
+    Nav,
+    DropdownButton,
+    MenuItem,
+    ButtonGroup,
+    Button
+} = require("react-bootstrap");
 
 var Store = require("../store");
 
 module.exports = React.createClass({
     mixins: [Store.mixin],
-    cursor: "users",
-    renderUsername(username, index) {
-        return <MenuItem eventKey={index}>{username}</MenuItem>;
-    },
     render() {
         return (
             <Navbar brand="Rust Playpen" inverse>
-                <Nav>
-                    <NavItem eventKey={1} href="#">Link</NavItem>
-                    <NavItem eventKey={2} href="#">Link</NavItem>
-                    <DropdownButton eventKey={3} title="Dropdown">
-                        {this.cursor.get().map(this.renderUsername)}
-                    </DropdownButton>
+                <Nav right>
+                    <ButtonGroup>
+                        <Button className="navbar-btn">Run</Button>
+                        <Button className="navbar-btn">Format</Button>
+                        <Button className="navbar-btn">Assembly</Button>
+                        <Button className="navbar-btn">LLVM IR</Button>
+                        <Button className="navbar-btn">Share</Button>
+                        <DropdownButton
+                            className="navbar-btn"
+                            title="Settings">
+                        </DropdownButton>
+                    </ButtonGroup>
                 </Nav>
             </Navbar>
         );
--- static/js/store.js
+++ static/js/store.js
@@ -1,8 +1,6 @@
 var baobab = require("baobab");
 
-var store = new baobab({
-    users: ["Gio", "Bob", "Sam", "HAHA"]
-});
+var store = new baobab({});
 
 module.exports = store;
 
